a0d2bb944dd2ed51e98e9452e61bcb7cd4b420f8
saharVac <vcsahar@gmail.com>
2020-11-19 13:53:23 -0400

    debugging

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 // Exporting an object containing all of our models
 
 module.exports = {
-  Exercise: require("./Exercise1"),
-  Workout: require("./Workout1")
+  Exercise: require("./exercise"),
+  Workout: require("./workout")
 };
